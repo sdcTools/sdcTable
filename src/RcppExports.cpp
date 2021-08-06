844169f6e385ecc905b7b1575adefc8d7a2b2952
Bernhard Meindl <bernhard.meindl@gmail.com>
2021-08-06 05:16:56 +0000

    updates due to rcpp

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // cpp_splitByIndices
 List cpp_splitByIndices(std::vector<std::string> strings, List strInfo);
 RcppExport SEXP _sdcTable_cpp_splitByIndices(SEXP stringsSEXP, SEXP strInfoSEXP) {
