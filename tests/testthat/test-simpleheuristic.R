fb01bd0545d63abdc28ae6ad3301e18c8fdfe27a
Bernhard Meindl <bernhard.meindl@statistik.gv.at>
2026-05-07 10:53:01 +0200

    remove ouput in test;

--- tests/testthat/test-simpleheuristic.R
+++ tests/testthat/test-simpleheuristic.R
@@ -75,7 +75,7 @@ test_that("simpleheuristic and simpleheuristic-old work", {
   expect_equal(sum(sdc_protected@results$sdcStatus == "x"), 0) # --> still a problem
 
   # we need to check that simpleheuristic adds at least one additional suppression
-  sdc_protected <- protectTable(sdc, method = "SIMPLEHEURISTIC", solve_attackerprobs = TRUE) # this is the default
+  sdc_protected <- protectTable(sdc, method = "SIMPLEHEURISTIC", solve_attackerprobs = TRUE, verbose = FALSE) # this is the default
   expect_equal(sum(sdc_protected@results$sdcStatus == "x"), 1)
 
   bnds <- attack(sdc_protected, to_attack = 8)
